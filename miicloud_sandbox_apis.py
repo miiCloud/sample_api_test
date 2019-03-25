39e68217d0a0c756da0af51f25e64b7121b981f5
Jag <42545981+jagbansal17@users.noreply.github.com>
2019-03-25 16:24:28 -0700

    Add files via upload

---
+++ miicloud_sandbox_apis.py
@@ -0,0 +1,100 @@
+from imutils import paths
+import cv2
+import requests
+import os
+import uuid
+import datetime
+
+"""
+    The following is miiCloud's publicly accessible Token.  If you would like to keep you test data separate, email dev@miicloud.io to get your unique Token.  There is no cost to get a token and use the miiCloud Sandbox environment.
+    """
+auth_token = 'Token 0e653da4969a757d21de0ffa6387d5fbd6401131'
+enroll_endpoint = 'http://sandbox.miicloud.io/miicloud/enroll'
+match_faces_endpoint = 'http://sandbox.miicloud.io/miicloud/match_faces'
+enrollment_image_dir = list(paths.list_images('input/persons'))
+test_image_dir = list(paths.list_images('input/test_images'))
+output_persons_path = 'output/persons/'
+output_test_img_path = 'output/test_images/'
+
+
+prev_name = ''
+##ENROLL VERIFIED PEOPLE IN THE DATABASE##
+#Go through each image, generate a unique_id per person and enroll all images in the person's folder
+for (i, image_path) in enumerate(enrollment_image_dir):
+    #Identify the name of the person being enrolled from folder structure
+    curr_name = image_path.split(os.path.sep)[-2]
+    
+    #Generate a unique identifier per person
+    if prev_name != curr_name:
+        unique_id = uuid.uuid4().hex
+        prev_name = curr_name
+
+    # Get the actual image from directory
+    filename = os.path.basename(image_path)
+    #print('image_path %s' % image_path)
+    #print('filename %s' % filename.split("."))
+    
+    #Build a JSON object with Unique Identifier and image
+    enrollment_payload = {'customer_person_id': unique_id}
+    file = {'image_path': (filename, open(image_path, 'rb'), "multipart/form-data")}
+
+    #Send the Enrollment payload to miiCloud
+    print("Enrolling image {}/{}".format(i + 1, len(enrollment_image_dir)) + " for " + curr_name + " w/ Unique ID-> " + unique_id)
+    enroll_response = requests.post(enroll_endpoint, headers={'Authorization': auth_token}, data = enrollment_payload, files = file)
+
+    #Check if the enrollment failed
+    if enroll_response.status_code != 200:
+        print('---API Error---')
+    #print(enroll_response.text)
+    #If enrollement is successfull, draw a bounding box around the face, and store the image in output/persons/ folder with timestamp
+    else:
+        print('  -Enrollment Successful!\n')
+        #print(enroll_response.json())
+        bounding_box = enroll_response.json()['bounding_box']
+        image = cv2.imread(image_path)
+        time_stamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        fn, ext = filename.split(".")
+        output_file = output_persons_path + fn + '_' + time_stamp + '.' + ext
+        cv2.rectangle(image, (bounding_box['left_x'], bounding_box['left_y']), (bounding_box['right_x'], bounding_box['right_y']), (255,255,0), 2)
+        cv2.imwrite(output_file, image)
+
+##MATCH FACES IN TEST IMAGES##
+for (i, image_path) in enumerate(test_image_dir):
+    
+    #Build a JSON object with Unique Identifier and image
+    filename = os.path.basename(image_path)
+    match_faces_payload = {'image_path': image_path}
+    file = {'image_path': (filename, open(image_path, 'rb'), "multipart/form-data")}
+    
+    #print('image_path %s' % image_path)
+    #print('filename %s' % filename.split("."))
+    
+    #Send the Payload to miiCloud
+    print("Processing test image {}/{}".format(i + 1, len(test_image_dir)))
+    match_faces_response = requests.post(match_faces_endpoint, headers={'Authorization': auth_token}, data = match_faces_payload, files = file)
+
+    #Check if the API request failed
+    if match_faces_response.status_code != 200:
+        print('---API Error---')
+    #If request is successful, get all the faces from API Response.  Draw a RED box around "unknown" faces and GREEN box around "known" faces.  Then save the image in output/test_images/ folder with timestamp
+    else:
+        print('  -Match Faces Complete!\n')
+        if len(match_faces_response.json()['faces']) > 0:
+            faces = match_faces_response.json()['faces']
+            #print(faces)
+            image = cv2.imread(image_path)
+            time_stamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            fn, ext = filename.split(".")
+            output_file = output_test_img_path + fn + '_' + time_stamp + '.' + ext
+            for face in faces:
+                bounding_box = face['bounding_box']
+                if face['match_status'] == 'unknown':
+                    cv2.rectangle(image, (bounding_box['left_x'], bounding_box['left_y']), (bounding_box['right_x'], bounding_box['right_y']), (0,0,255), 2)
+                else:
+                    cv2.rectangle(image, (bounding_box['left_x'], bounding_box['left_y']), (bounding_box['right_x'], bounding_box['right_y']), (0,255,0), 2)
+                    cv2.rectangle(image, (bounding_box['left_x'],bounding_box['right_y']),(bounding_box['right_x'], bounding_box['right_y']+20), (0,255,0), -1)
+                    cv2.putText(image, 'Known', (bounding_box['left_x'],bounding_box['right_y']+18), cv2.cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
+                  
+            cv2.imwrite(output_file, image)
+        
+
